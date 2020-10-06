5a5000274f93710005635de6ae508363b21fb826
Radoslaw Mazurczak <RMazurczak@protonmail.com>
2020-10-06 11:33:49 +0200

    Add menu

---
+++ src/components/menuBox.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="box boxBlind" :id="index" >
+    <div @click="close()">
+      <img src='../assets/box.png'/>
+      <div class="string" >{{msg}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'menuBox',
+  data() {
+    return {
+    };
+  },
+  props: ['index', 'msg'],
+  mounted() {
+    this.flash();
+  },
+  methods: {
+    flash() {
+      const name = `${this.index}`;
+      document.getElementById(name).style.opacity = '0';
+      setTimeout(() => {
+        document.getElementById(name).style.opacity = '1';
+      }, 3600);
+    },
+    close() {
+      if (this.index === 'box4') {
+        window.close();
+      }
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+.box>div>img{
+  height: 3.5em;
+  -webkit-filter: drop-shadow(4px 8px 2px #000000);
+  filter: drop-shadow(4px 8px 2px #000000);
+  z-index: 7;
+  position: relative;
+  transition: 0.1s;
+  margin-bottom: 2em;
+  opacity: 1;
+  &:hover{
+    transform: scale(1.05,1.05);
+    cursor: pointer;
+  }
+}
+.box{
+  text-align: center;
+}
+.boxNow{
+  opacity: 1;
+  animation: bum 1s 1;
+}
+.boxBlind{
+  opacity: 1;
+}
+@keyframes bum {
+  0%{
+    opacity: 0;
+  }
+  100%{
+    opacity: 1;
+  }
+}
+.string{
+  position: fixed;
+  z-index: 7;
+  height: 0;
+  font-family: 'Bebas Neue', cursive;
+  font-size: 2em;
+  letter-spacing: 0.3em;
+  margin-left: 50%;
+  transform: translate(-50%, -50%);
+  margin-top: -2.5em;
+  color: whitesmoke;
+  text-shadow: 3px 3px 0px rgba(0, 0, 0, 1);
+  margin-bottom: 1.5em;
+  cursor: pointer;
+}
+.box>div:hover{
+  img{
+    transform: scale(1.05,1.05);
+  }
+}
+</style>
---
+++ src/views/Menu.vue
@@ -0,0 +1,88 @@
+<template>
+  <div >
+    <img class="logo" id="logo" src='../assets/logo.png'/>
+    <Box index="box0" msg="START"/>
+    <Box index="box1" msg="continue"/>
+    <Box index="box2" msg="settings"/>
+    <Box index="box3" msg="credits"/>
+    <Box index="box4" msg="exit"/>
+    <div id="flash" class="flash"></div>
+  </div>
+</template>
+
+<script>
+import Box from '@/components/menuBox.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Box,
+  },
+  data() {
+    return {
+    };
+  },
+  mounted() {
+    this.flash();
+    this.music();
+  },
+  methods: {
+    flash() {
+      setTimeout(() => {
+        document.getElementById('flash').classList.add('flashNow');
+      }, 3000);
+      document.getElementById('logo').style.opacity = '0';
+      setTimeout(() => {
+        document.getElementById('logo').style.opacity = '1';
+      }, 3600);
+    },
+    music() {
+      // eslint-disable-next-line global-require
+      const audio = new Audio(require('@/assets/music.mp3'));
+      audio.play();
+      audio.volume = 0.02;
+    },
+  },
+};
+</script>
+<style style="scss">
+body{
+  height: 100%;
+  background: url('../assets/menu.jpg') no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  text-align: center;
+  z-index: 0;
+  font-size: 2vh;
+}
+.flash{
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    z-index: 5;
+    top:0;
+    left: 0;
+}
+.flashNow{
+    animation: bum 1s 1;
+}
+@keyframes bum {
+  0%{
+    background-color: transparent;
+  }
+  50%{
+    background-color: white;
+  }
+  100%{
+  background-color: transparent;
+  }
+}
+.logo{
+  width:20em;
+  height: auto;
+  -webkit-filter: drop-shadow(4px 8px 2px #000000);
+  filter: drop-shadow(4px 8px 2px #000000);
+}
+</style>
